8c0c569a91d7f4ce92ca711ab50ba6e9302b7264
Joe Heinz <joeheinz99@gmail.com>
2022-07-18 22:34:05 -0400

    riot txt security

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ valorantRouter.get('/:riotId/:tagLine', valorantController.valData, (req, res) =
 // app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../dist")));
-  app.get('/riot.txt', (req, res) => {
+  app.get('//riot.txt', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../riot.txt"));
   });
 
