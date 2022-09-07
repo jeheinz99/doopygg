dda2a655ba929d1de6ac6bad9dcdca771719f28f
Joe Heinz <joeheinz99@gmail.com>
2022-09-07 01:37:50 -0400

    slight auth changes

--- server/routers/riotAuth.js
+++ server/routers/riotAuth.js
@@ -1,17 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const authController = require('../controllers/authController');
 
 // AUTH ENDPOINT
 // router handler to handle all requests to authentication
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
-router.get('/auth/callback', authController.getAuthTokens, authController.setTokenCookies, (req, res) => {
-  return res.status(300).redirect('https://www.doopy.dev/valorant');
+const appBaseUrl = 'http://www.doopy.dev';
+const appCallbackUrl = `${appBaseUrl}/riot/auth/callback`;
+
+const provider = 'https://auth.riotgames.com'
+const authorizeUrl = `${provider}/authorize`;
+const tokenUrl = `${provider}/token`;
+
+const redirect_uri = 'http://www.doopy.dev/riot/auth';
+
+router.get('/auth/callback', (req, res) => {
+  const accessCode = req.query.code;
+
+  // make server-to-server request to token endpoint
+  // exchange auth code for tokens
+  request.post({
+    url: tokenUrl,
+    auth: { // sets "Authorization: Basic ..." header
+      user: clientID,
+      pass: clientSecret
+    },
+    form: { // post information as form-data
+      grant_type: "authorization_code",
+      code: accessCode,
+      redirect_uri: appCallbackUrl
+    }
+  }, 
+  (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      // parse the response to JSON
+      const payload = JSON.parse(body);
+      // separate the tokens from the entire response body
+      const tokens = {
+        refresh_token: payload.refresh_token,
+        id_token: payload.id_token,
+        access_token: payload.access_token
+      };
+
+      // Legibly print out our tokens
+      res.send("<pre>" + JSON.stringify(tokens, false, 4) + "</pre");
+    } else {
+      res.send("/token request failed");
+    }
+  });
 });
 
-router.get('/auth', authController.getAuthLink, (req, res) => {
-  return res.status(200).send(res.locals.authLink)
+router.get('/auth', (req, res)=> { 
+  const link = `${authorizeUrl}?redirect_uri=${appCallbackUrl}&client_id=${clientID}&response_type=code&scope=openid`;
+  res.send('<a href="' + link + '"> Sign In</a>');
 });
 
 module.exports = router;
